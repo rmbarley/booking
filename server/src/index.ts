1366cb76c72518a81650cdc3128588b83b0c9633
Ryan Barley <rmbarley@gmail.com>
2024-01-23 23:14:43 -0500

    Implement hotel routes in backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import path from "path";
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-
+import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth";
 import userRoutes from "./routes/users";
+import myHotelsRoutes from "./routes/my-hotels";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -20,6 +27,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/my-hotels", myHotelsRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
